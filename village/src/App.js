b0c63490cb1b4013a76d65e46d5dd9653e7373d7
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-03-01 11:06:39 -0500

    initial commit on correct branch haha

--- village/src/App.js
+++ village/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
- 
+  
 class App extends Component {
   constructor(props) {
     super(props);
